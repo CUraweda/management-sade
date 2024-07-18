c947f8c7f5a62aa6e9113399135ff9b27d5be5d6
Razan Muhammad <razanmfs507@gmail.com>
2024-07-18 18:22:49 +0700

    fix paginate

--- src/midleware/Api.ts
+++ src/midleware/Api.ts
@@ -45,17 +45,6 @@ const DaftarDataPetugas = {
       },
     }),
 
-  GetAllDataPetugas: (
-    token: string | null
-  ): AxiosPromise<DataPetugasResponse> =>
-    instance({
-      method: "GET",
-      url: `/api/waste-officer`,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }),
-
   PosDataPetugas: (token: string | null, data: any): AxiosPromise<any> =>
     instance({
       method: "POST",
@@ -138,7 +127,7 @@ const BankSampah = {
   ): AxiosPromise<WasteCollectionResponse> =>
     instance({
       method: "GET",
-      url: `/api/waste-collection/get-by-filter?waste_type_id=${wasteId}&class_id=${classId}&start_date=${fromDate}&end_date=${toDate}`,
+      url: `/api/waste-collection/get-by-filter?waste_type_id=${wasteId}&class_id=${classId}&start_date=${fromDate}&end_date=${toDate}&limit=100000000`,
       headers: {
         Authorization: `Bearer ${token}`,
       },
