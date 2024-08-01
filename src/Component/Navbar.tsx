e4fba98cc24c82804ac6b61c75bbe14f01243c44
Razan Muhammad <razanmfs507@gmail.com>
2024-08-01 11:31:34 +0700

    add avatar on navbar

--- src/Component/Navbar.tsx
+++ src/Component/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
               <div className="w-10 rounded-full">
                 <img
                   alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                  src="https://thinksport.com.au/wp-content/uploads/2020/01/avatar-.jpg"
                 />
               </div>
             </div>
